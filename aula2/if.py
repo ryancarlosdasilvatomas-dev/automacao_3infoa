96e1a0dee04b45a12c2aff7dd33edf58773f69db
ryancarlosdasilvatomas-dev <ryancarlosdasilvatomas@gmail.com>
2026-03-16 10:37:11 -0300

    Aula 2 - Estrutura de Condição

---
+++ aula2/if.py
@@ -0,0 +1,17 @@
+
+#Entrada
+titulo = input("Digite o título do filme: ")
+diarias = int(input("Por quantos dias alugou o filme?"))
+tempo = int(input("Por quantos dias ficou com o filme?"))
+
+#Processamento
+valor = tempo * 5
+multa = 0
+
+if tempo - diarias > 0:
+    multa = 15
+
+total = valor + multa
+
+#Saída
+print("Valor a ser pago: R$ ", total)
---
+++ aula2/if_else.py
@@ -0,0 +1,12 @@
+#Estrutura de condição
+#IF e Else
+
+#Entrada
+#Variável + conversão de Triplo (Float) + Entrada de Dados (Input)
+media = float(input("Digite sua média"))
+
+#Processamento
+if media >= 6:
+    print("Aprovado")
+else: 
+    print("Reprovado") 
